9f6f1b9ca6d21f548cc48d8303debb2589b125f9
Gokce Toykuyu <gokce@verizonmedia.com>
2020-04-29 18:21:14 -0700

    Assigning request count attribute to a static field

--- src/main/java/org/eclipse/jetty/server/listener/LimitedRequestListener.java
+++ src/main/java/org/eclipse/jetty/server/listener/LimitedRequestListener.java
@@ -7,9 +7,11 @@ import org.eclipse.jetty.util.log.Logger;
 
 public class LimitedRequestListener implements HttpChannel.Listener {
 
+	private static final Logger LOG = Log.getLogger(LimitedRequestListener.class);
+
 	private static final String CONN_CLOSE_HEADER_NAME = "Connection";
 	private static final String CONN_CLOSE_HEADER_VAL = "Close";
-	private static final Logger LOG = Log.getLogger(LimitedRequestListener.class);
+	private static final String JETTY_REQ_COUNT_ATTRIB = "org.eclipse.jetty.LimitedRequestListener.RequestCount";
 
 	private int maxRequests = 10;
 
@@ -24,26 +26,34 @@ public class LimitedRequestListener implements HttpChannel.Listener {
 	@Override
 	public void onRequestBegin(Request request) {
 		// set the number of requests as an attribute to help correlate exceptions to closed connections
-		request.setAttribute("org.eclipse.jetty.LimitedRequestListener.RequestCount", request.getHttpChannel().getRequests());
+		request.setAttribute(JETTY_REQ_COUNT_ATTRIB, getRequestCount(request));
 	}
 
 	@Override
 	public void onResponseBegin(Request request) {
-		long requests = request.getHttpChannel().getRequests();
+		long requests = getRequestCount(request);
 		// After X responses, forcibly set connection close on response
 		if (requests >= maxRequests) {
 			request.getResponse().setHeader(CONN_CLOSE_HEADER_NAME, CONN_CLOSE_HEADER_VAL);
-			LOG.debug("Setting [Connection: Close] on Request #{} for {}", requests, request.getHttpChannel().getEndPoint().getTransport());
+			if (LOG.isDebugEnabled()) {
+				LOG.debug("Setting [Connection: Close] on Request #{} for {}", requests, request.getHttpChannel().getEndPoint().getTransport());
+			}
 		}
 	}
 
 	@Override
 	public void onComplete(Request request) {
-		long requests = request.getHttpChannel().getRequests();
+		long requests = getRequestCount(request);
 		// forcibly close connection after X request and responses are complete
 		if (requests > maxRequests) {
 			request.getHttpChannel().getEndPoint().close();
-			LOG.debug("Forcing Close on Request #{} for {}", requests, request.getHttpChannel().getEndPoint().getTransport());
+			if (LOG.isDebugEnabled()) {
+				LOG.debug("Forcing Close on Request #{} for {}", requests, request.getHttpChannel().getEndPoint().getTransport());
+			}
 		}
+	}
+
+	private static long getRequestCount(Request request) {
+		return request.getHttpChannel().getRequests();
 	}
 }
